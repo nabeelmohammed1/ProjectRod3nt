e12c30b79d1257d07a3a05b9b2cfc011a1045e6c
NotJonatan <60527758+NotJonatan@users.noreply.github.com>
2026-07-14 20:22:26 -0400

    making progress with caching

---
+++ PSOCollection/GeneratePSOBundles.bat
@@ -0,0 +1,3 @@
+"C:\Program Files\Epic Games\UE_5.6\Engine\Binaries\Win64\UnrealEditor-Cmd.exe" -run=ShaderPipelineCachetools expand "D:\College\GAME 405\Github\ProjectRod3nt\PSOCollection\*.rec.upipelinecache" "D:\College\GAME 405\Github\ProjectRod3nt\PSOCollection\*.shk" "D:\College\GAME 405\Github\ProjectRod3nt\PSOCollection\PSO_PCD3D_SM6.spc"
+
+cmd /k
